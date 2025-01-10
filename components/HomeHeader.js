079ab2cb850cc4e0ae4ab1c376d728f75e1d3346
Ben Ong <ong.ben@gmail.com>
2025-01-09 23:02:21 -0800

    Customized HomeHeader

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,24 +1,35 @@
 import { Platform, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { Image } from 'expo-image'
+import { blurhash } from '../utils/common'
+import { useAuth } from '../context/authContext'
 
 const ios = Platform.OS == 'ios';
 
 export default function HomeHeader() {
   const { top } = useSafeAreaInsets();
+  const { user } = useAuth();
 
   return (
     <View
       style={{
         paddingTop: ios ? top : top+10,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        backgroundColor: 'lightblue',
+        backgroundColor: '#3B94CB',
         paddingHorizontal: 16,
         paddingBottom: 16,
         borderRadius: 20
       }}>
-      <Text style={{ fontSize: 20, color: 'white', fontWeight: 'bold' }}>Chats</Text>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+        <Text style={{ fontSize: 20, color: 'white', fontWeight: 'bold' }}>Chats</Text>
+        <Image
+          style={{ height: 32, aspectRatio: 1, borderRadius: 100 }}
+          // source="https://picsum.photos/seed/696/3000/2000"
+          source={user?.profileUrl}
+          placeholder={{ blurhash }}
+          transition={500}
+        />
+      </View>
     </View>
   )
 }
